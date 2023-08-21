884108b62fbd06eed8ac45598acf992cd07a7121
Leanderson Gustavo S Silva <lssilva@ceapema.matriz>
2023-08-21 14:57:10 -0300

    Implementação Charts

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -1,4 +1,6 @@
-﻿namespace CentralInovacao;
+﻿using Microcharts.Maui;
+
+namespace CentralInovacao;
 
 public static class MauiProgram
 {
@@ -7,6 +9,7 @@ public static class MauiProgram
 		var builder = MauiApp.CreateBuilder();
 		builder
 			.UseMauiApp<App>()
+			.UseMicrocharts()
 			.ConfigureFonts(fonts =>
 			{
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
--- Views/ViewEsteiraAcompanhamento.xaml.cs
+++ Views/ViewEsteiraAcompanhamento.xaml.cs
@@ -1,9 +1,96 @@
+using Microcharts;
+using Microcharts.Maui;
+using SkiaSharp;
+
 namespace CentralInovacao.Views;
 
 public partial class ViewEsteiraAcompanhamento : ContentPage
 {
-	public ViewEsteiraAcompanhamento()
-	{
-		InitializeComponent();
-	}
+    //Fonte de Dados Hard-Coded
+    ChartEntry[] entries = new[]
+    {
+        new ChartEntry(212)
+        {
+            Label= "Leanderson",
+            ValueLabel = "112",
+            Color=SKColor.Parse("#2c3e50")
+        },
+        new ChartEntry(248)
+        {
+            Label= "Edem",
+            ValueLabel = "648",
+            Color=SKColor.Parse("#77d065")
+        },
+        new ChartEntry(128)
+        {
+            Label= "Danilo",
+            ValueLabel = "428",
+            Color=SKColor.Parse("#b455b6")
+        },
+        new ChartEntry(514)
+        {
+            Label= "Bruce",
+            ValueLabel = "214",
+            Color=SKColor.Parse("#3498db")
+        }
+    };
+    ChartEntry[] entries2 = new[]
+{
+        new ChartEntry(212)
+        {
+            Label= "Finalizadas",
+            ValueLabel = "10",
+            Color=SKColor.Parse("#2c3e50")
+        },
+        new ChartEntry(248)
+        {
+            Label= "Pendentes",
+            ValueLabel = "13",
+            Color=SKColor.Parse("#77d065")
+        }
+    };
+    ChartEntry[] entries3 = new[]
+{
+        new ChartEntry(212)
+        {
+            Label= "Infra",
+            ValueLabel = "112",
+            Color=SKColor.Parse("#2c3e50")
+        },
+        new ChartEntry(248)
+        {
+            Label= "Inovação",
+            ValueLabel = "648",
+            Color=SKColor.Parse("#77d065")
+        },
+        new ChartEntry(128)
+        {
+            Label= "TI Sistemas",
+            ValueLabel = "428",
+            Color=SKColor.Parse("#b455b6")
+        }
+    };
+    public ViewEsteiraAcompanhamento()
+    {
+        InitializeComponent();
+
+        _chartView.Chart = new BarChart
+        {
+            Entries = entries, //Fonte de Dados
+            LabelOrientation = Orientation.Horizontal,
+            ValueLabelOrientation = Orientation.Horizontal,
+        };
+        _chartView1.Chart = new DonutChart
+        {
+            Entries = entries2, //Fonte de Dados
+            LabelMode = LabelMode.RightOnly                  
+        };
+
+        _chartView2.Chart = new PieChart 
+        { 
+            Entries = entries3,
+            LabelMode = LabelMode.RightOnly
+        };
+
+    }
 }
