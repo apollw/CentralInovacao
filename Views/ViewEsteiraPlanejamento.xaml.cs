00fbee2f6b1aa9cf6b70645c2fadac8274178658
Leanderson Gustavo S Silva <lssilva@ceapema.matriz>
2023-08-22 12:35:21 -0300

    Esteira de Acompanhamento

--- Views/ViewEsteiraPlanejamento.xaml.cs
+++ Views/ViewEsteiraPlanejamento.xaml.cs
@@ -1,9 +1,39 @@
+using Microsoft.Maui.Controls;
+using System.Collections.ObjectModel;
+
 namespace CentralInovacao.Views;
 
 public partial class ViewEsteiraPlanejamento : ContentPage
 {
-	public ViewEsteiraPlanejamento()
+    public ObservableCollection<string> Items { get; set; }
+
+    public ViewEsteiraPlanejamento()
 	{
 		InitializeComponent();
-	}
+
+        // Inicialize a coleção de itens e adicione alguns exemplos
+        Items = new ObservableCollection<string>
+            {
+                "Tarefa 1",
+                "Tarefa 2",
+                "Tarefa 3",
+                "Tarefa 1",
+                "Tarefa 2",
+                "Tarefa 3",
+                "Tarefa 1",
+                "Tarefa 2",
+                "Tarefa 3",
+                "Tarefa 1",
+                "Tarefa 2",
+                "Tarefa 3",
+                "Tarefa 1",
+                "Tarefa 2",
+                "Tarefa 3"
+            };
+
+        // Vincule a coleção à CollectionView
+        _collectionView.ItemsSource  = Items;
+        _collectionView1.ItemsSource = Items;
+        _collectionView2.ItemsSource = Items;
+    }
 }
