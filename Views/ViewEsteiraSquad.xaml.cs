8ffeb13cae58907fff6e38181b270a788b95f631
Leanderson Gustavo S Silva <lssilva@ceapema.matriz>
2023-08-29 14:56:49 -0300

    Grupo

--- Views/ViewEsteiraSquad.xaml.cs
+++ Views/ViewEsteiraSquad.xaml.cs
@@ -6,4 +6,15 @@ public partial class ViewEsteiraSquad : ContentPage
 	{
 		InitializeComponent();
 	}
+    private async void Btn_AddColaborador(object sender, EventArgs e)
+    {
+        var button = (Button)sender;
+        int animationDuration = 100;
+
+        //Btn_Animation(button);
+        await Task.Delay(animationDuration / 2);
+
+        //await Navigation.PushAsync(new ViewNovaOportunidade());
+        //await Shell.Current.GoToAsync($"{nameof(ViewNovaOportunidade)}");
+    }
 }
